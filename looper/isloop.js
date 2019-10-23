268e9a59dae3b42d71bf5f468cc1f07174ccb9c9
Lillian Moriarty <lilybmoriarty@gmail.com>
2019-10-23 15:56:42 -0400

    finished looper

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,47 @@
-'use strict';
+"use strict";
 
 //Complete this algo
-const isLoop = (linkedlist) => {
+const isLoop = linkedlist => {
+  let pointer = linkedlist.head;
+  let prev = {
+    // 1: true,
+    // 2: true,
+    // 3: true
+  };
 
+  if (!pointer) return false;
+
+  while (pointer.next) {
+    prev[pointer.value] = true;
+    if (prev[pointer.next.value]) {
+      return true;
+    }
+    pointer = pointer.next;
+  }
+
+  return false;
 };
 
+const findLoop = linkedlist => {
+  let pointer = linkedlist.head;
+  let prev = {
+    // 1: true,
+    // 2: true,
+    // 3: true
+  };
+
+  if (!pointer) return "there is no loop, silly.";
+
+  while (pointer.next) {
+    prev[pointer.value] = true;
+    if (prev[pointer.next.value]) {
+      return pointer.value;
+    }
+    pointer = pointer.next;
+  }
+
+  return "there is no loop, silly.";
+};
 
 /*
 EXTRA CREDIT:
@@ -14,4 +51,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = { isLoop, findLoop };
--- looper/isloop.spec.js
+++ looper/isloop.spec.js
@@ -1,38 +1,61 @@
-'use strict';
-const {expect} = require('chai');
-const LinkedList = require('./linkedlist');
-const isLoop = require('./isloop');
+"use strict";
+const { expect } = require("chai");
+const LinkedList = require("./linkedlist");
+const { isLoop, findLoop } = require("./isloop");
 
-const numbers = [1,2,3,4,5,6,7,8,9,10];
-const letters = ['a','b','c','d','e','f','g','h','i'];
-const emojis = ['🥚','🐣','🦅 ', '👾'];
-
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+const emojis = ["🥚", "🐣", "🦅 ", "👾"];
 
 //These LLs have loops!
 const test1 = new LinkedList();
-test1.addToHead(0)
+test1.addToHead(0);
 numbers.forEach(val => test1.addToTail(val));
 test1.getNthNode(6).next = test1.getNthNode(3);
 
 const test2 = new LinkedList();
-test2.addToHead('z');
+test2.addToHead("z");
 letters.forEach(letter => test2.addToTail(letter));
 test2.getNthNode(9).next = test2.getNthNode(1);
 
 // This LL has no loop!
 const test3 = new LinkedList();
-test3.addToHead('📺');
-emojis.forEach(el => test3.addToTail(el))
+test3.addToHead("📺");
+emojis.forEach(el => test3.addToTail(el));
 
-describe('isLoop function', () => {
-	
-	// Feel free to write a few tests of your own! :)
-	it('returns true if linked list contains a loop', () =>{
-		expect(isLoop(test1)).to.equal(true);
-		expect(isLoop(test2)).to.equal(true);
-	});
+// This is the single node
+const test4 = new LinkedList();
+test4.addToHead("lily");
 
-	it('returns false if linked list contains no loop', () =>{
-		expect(isLoop(test3)).to.equal(false);
-	});
+// This is the no node
+const test5 = new LinkedList();
+
+describe("isLoop function", () => {
+  // Feel free to write a few tests of your own! :)
+  it("returns true if linked list contains a loop", () => {
+    expect(isLoop(test1)).to.equal(true);
+    expect(isLoop(test2)).to.equal(true);
+  });
+
+  it("returns false if linked list contains no loop", () => {
+    expect(isLoop(test3)).to.equal(false);
+  });
+
+  it("returns false if linked list has only one node", () => {
+    expect(isLoop(test4)).to.equal(false);
+  });
+
+  it("returns false if linked list has no nodes ;(", () => {
+    expect(isLoop(test5)).to.equal(false);
+  });
+});
+
+describe("findLoop function", () => {
+  // Feel free to write a few tests of your own! :)
+  it("returns the node value if the linkedlist contains a loop", () => {
+    expect(findLoop(test1)).to.equal(5);
+  });
+  it("returns a helpful message if there is no loop in the linkedlist", () => {
+    expect(findLoop(test3)).to.equal("there is no loop, silly.");
+  });
 });
